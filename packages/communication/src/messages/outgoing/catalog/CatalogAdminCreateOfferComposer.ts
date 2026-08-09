ed5649421f7429b8eaa35ff91c3334613ea8553a
simoleo89 <11816867+simoleo89@users.noreply.github.com>
2026-08-09 21:20:40 +0200

    feat(catalog): add Smart Save protocol

--- packages/communication/src/messages/outgoing/catalog/CatalogAdminCreateOfferComposer.ts
+++ packages/communication/src/messages/outgoing/catalog/CatalogAdminCreateOfferComposer.ts
@@ -4,9 +4,9 @@ export class CatalogAdminCreateOfferComposer implements IMessageComposer<Constru
 {
     private _data: ConstructorParameters<typeof CatalogAdminCreateOfferComposer>;
 
-    constructor(pageId: number, itemIds: string, catalogName: string, costCredits: number, costPoints: number, pointsType: number, amount: number, clubOnly: number, extradata: string, haveOffer: boolean, offerIdGroup: number, limitedStack: number, orderNumber: number, songId: number = 0, catalogMode: string = 'NORMAL', draftVersionId: number = 0, expectedRevision: number = 0, lockToken: string = '', summary: string = '')
+    constructor(pageId: number, itemIds: string, catalogName: string, costCredits: number, costPoints: number, pointsType: number, amount: number, clubOnly: number, extradata: string, haveOffer: boolean, offerIdGroup: number, limitedStack: number, orderNumber: number, songId: number = 0, catalogMode: string = 'NORMAL', draftVersionId: number = 0, expectedRevision: number = 0, lockToken: string = '', summary: string = '', operationId: string = '')
     {
-        this._data = [ pageId, itemIds, catalogName, costCredits, costPoints, pointsType, amount, clubOnly, extradata, haveOffer, offerIdGroup, limitedStack, orderNumber, songId, catalogMode, draftVersionId, expectedRevision, lockToken, summary ];
+        this._data = [ pageId, itemIds, catalogName, costCredits, costPoints, pointsType, amount, clubOnly, extradata, haveOffer, offerIdGroup, limitedStack, orderNumber, songId, catalogMode, draftVersionId, expectedRevision, lockToken, summary, operationId ];
     }
 
     dispose(): void
